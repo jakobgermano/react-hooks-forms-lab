32f6f601998dda2296b9425925e5a7f84a2d3bf6
jakob <jakob.germano@gmail.com>
2021-08-18 18:49:40 -0600

    as much as i could figure out

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
+import {useState} from 'react'
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [newItem, setNewItem] = useState("")
+  const [cat, setCat] = useState("Produce")
+  const [name, setName] = useState("")
+
+  function handleNameChange(e){
+    console.log(e.target)
+    setName(e.target.value)
+  }
+
+  function handleCatChange(e){
+    console.log(e.target)
+    setCat(e.target.value)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleCatChange} value={cat}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,22 +3,36 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
+
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  const [allData, setAllData] = useState(items);
+  function onItemFormSubmit(itemNew) {
+    setAllData([...allData, itemNew]);
+  }
 
   const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = allData.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const [searchChange, setSearchChange] = useState("");
+  const handleSearchChange = (e) => setSearchChange(e.target.value);
+
+  const searchItemsToDisplay = itemsToDisplay.filter((item) =>
+    item.name.toLowerCase().includes(searchChange.toLowerCase())
+  );
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
       <Filter onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
@@ -28,5 +42,6 @@ function ShoppingList({ items }) {
     </div>
   );
 }
+  )}
 
 export default ShoppingList;
